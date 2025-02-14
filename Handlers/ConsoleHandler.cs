2636568ae50f38eb792bd4b23ae0aa1c7599f885
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-02-14 15:03:06 -0500

    Refactor ConsoleHandler for improved readability and consistency
    Reorganized declarations and adjusted list initialization for clarity. Simplified conditional blocks and appended methods for string formatting. Minor changes enhance code maintainability and ensure consistent conventions.

--- Handlers/ConsoleHandler.cs
+++ Handlers/ConsoleHandler.cs
@@ -12,9 +12,9 @@ internal class ConsoleHandler
     private const ConsoleColor MESSAGE_COLOR = ConsoleColor.Blue;
     private const ConsoleColor PROGRESS_COLOR = ConsoleColor.Yellow;
     private const ConsoleColor WARN_COLOR = ConsoleColor.DarkYellow;
-    private static ConsoleHandler Instance = Instance ??= new();
-    private readonly List<string> messages;
+    private static readonly ConsoleHandler Instance = Instance ??= new ConsoleHandler();
     private readonly List<string> errors;
+    private readonly List<string> messages;
     private readonly long start_time;
     private bool show_stats;
 
@@ -34,26 +34,22 @@ internal class ConsoleHandler
     {
         Instance.messages.Add(messages);
         if (duration != null)
-        {
             Task.Run(() =>
             {
                 Thread.Sleep((int)duration.Value.TotalMilliseconds);
                 Instance.messages.Remove(messages);
             });
-        }
     }
 
     public static void SendError(string messages, TimeSpan? duration = null)
     {
         Instance.errors.Add(messages);
         if (duration != null)
-        {
             Task.Run(() =>
             {
                 Thread.Sleep((int)duration.Value.TotalMilliseconds);
                 Instance.errors.Remove(messages);
             });
-        }
     }
 
     public static void ShowStats()
@@ -68,13 +64,9 @@ internal class ConsoleHandler
     public static void ToggleStatsVisibility()
     {
         if (Instance.show_stats)
-        {
             HideStats();
-        }
         else
-        {
             ShowStats();
-        }
     }
 
     private static string GetTime(TimeSpan span)
@@ -82,20 +74,28 @@ internal class ConsoleHandler
         StringBuilder time_builder = new();
         if (span.Days > 0)
         {
-            time_builder.Append($"{span.Days} days ");
+            time_builder.Append(span.Days);
+            time_builder.Append(" days ");
         }
+
         if (span.Hours > 0)
         {
-            time_builder.Append($"{span.Hours} hours ");
+            time_builder.Append(span.Hours);
+            time_builder.Append(" hours ");
         }
+
         if (span.Minutes > 0)
         {
-            time_builder.Append($"{span.Minutes} minutes ");
+            time_builder.Append(span.Minutes);
+            time_builder.Append(" minutes");
         }
+
         if (span.Seconds > 0)
         {
-            time_builder.Append($"{span.Seconds} seconds");
+            time_builder.Append(span.Seconds);
+            time_builder.Append(" seconds");
         }
+
         return time_builder.ToString();
     }
 
@@ -129,13 +129,13 @@ internal class ConsoleHandler
         Console.ForegroundColor = MESSAGE_COLOR;
         Console.Write("Runtime: ");
         Console.ForegroundColor = ACCENT_COLOR;
-        Console.WriteLine(GetTime(new(DateTime.Now.Ticks - start_time)));
+        Console.WriteLine(GetTime(new TimeSpan(DateTime.Now.Ticks - start_time)));
 
         // Needs Refresh
         Console.ForegroundColor = MESSAGE_COLOR;
         Console.Write("Needs Refresh: ");
         Console.ForegroundColor = ACCENT_COLOR;
-        Console.WriteLine(GetTime(new(DateTime.Now.Ticks - start_time)));
+        Console.WriteLine(GetTime(new TimeSpan(DateTime.Now.Ticks - start_time)));
     }
 
     private void UpdateTitle()
@@ -143,8 +143,8 @@ internal class ConsoleHandler
         Console.ForegroundColor = ConsoleColor.Green;
         Console.Write("PROCESSING: ");
 
-        string path = new DirectoryInfo(Environment.CurrentDirectory).Name;
-        int path_length = 20;
+        var path = new DirectoryInfo(Environment.CurrentDirectory).Name;
+        var path_length = 20;
 
         string title;
         if (Environment.CurrentDirectory.Length >= path_length + path.Length + 4)
